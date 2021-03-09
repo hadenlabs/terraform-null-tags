8d3234b82265e09488bc13b362cc02241d7a3fd8
@slovacus <slovacus@gmail.com>
2021-03-09 04:19:58 -0500

    refactor: output_data to outputs (#3)

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "namespace" {
-  value       = local.output_data.namespace
+  value       = local.outputs.namespace
   description = "Normalized namespace"
 }
 
 output "environment" {
-  value       = local.output_data.environment
+  value       = local.outputs.environment
   description = "Normalized environment"
 }
 
 output "name" {
-  value       = local.output_data.name
+  value       = local.outputs.name
   description = "Normalized name"
 }
 
 output "stage" {
-  value       = local.output_data.stage
+  value       = local.outputs.stage
   description = "Normalized stage"
 }
 
 output "delimiter" {
-  value       = local.output_data.delimiter
+  value       = local.outputs.delimiter
   description = "Delimiter between `namespace`, `environment`, `stage`, `name` and `attributes`"
 }
 
@@ -34,16 +34,16 @@ output "name32" {
 }
 
 output "attributes" {
-  value       = local.output_data.attributes
+  value       = local.outputs.attributes
   description = "List of attributes"
 }
 
 output "label_order" {
-  value       = local.output_data.label_order
+  value       = local.outputs.label_order
   description = "The naming order actually used to create the ID"
 }
 
 output "tags" {
   description = "Tags as a map (includes a `Name` tag)"
-  value       = local.output_data.tags
+  value       = local.outputs.tags
 }
