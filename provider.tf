db5326921c475b82bce065b0c338a70af6a5586b
@slovacus <slovacus@gmail.com>
2021-04-10 14:29:46 -0500

    fix: generate outputs tags (#5)

--- provider.tf
+++
@@ -1,1 +0,0 @@
-provider "locals" {}
--- variables.tf
+++ variables.tf
@@ -1,16 +1,11 @@
 variable "config" {
-  type = any
-  default = {
-    delimiter           = null
-    regex_replace_chars = null
-    label_order         = []
-    label_key_case      = null
-    label_value_case    = null
-  }
+  type        = any
   description = <<-EOT
     Single object for setting entire configuration at once.
     See description of individual variables for details.
   EOT
+
+  default = {}
 
   validation {
     condition     = lookup(var.config, "label_key_case", null) == null ? true : contains(["lower", "title", "upper"], var.config["label_key_case"])
@@ -25,36 +20,32 @@ variable "config" {
 
 variable "namespace" {
   type        = string
-  default     = null
   description = "Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'"
 }
 
 variable "environment" {
   type        = string
-  default     = null
   description = "Environment, e.g. 'uw2', 'us-west-2', OR 'prod', 'staging', 'dev', 'UAT'"
 }
 
 variable "stage" {
   type        = string
-  default     = null
   description = "Stage, e.g. 'prod', 'staging', 'dev', OR 'source', 'build', 'test', 'deploy', 'release'"
 }
 
 variable "name" {
   type        = string
-  default     = null
   description = "Solution name, e.g. 'app' or 'jenkins'"
 }
 
 variable "attributes" {
   type        = list(string)
-  default     = []
   description = "Additional attributes (e.g. `1`)"
+  default     = []
 }
 
 variable "tags" {
   type        = map(string)
-  default     = {}
   description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`"
+  default     = {}
 }
