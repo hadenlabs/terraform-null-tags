1c572502f5ed6fd299048cb87ff090b887c54903
Luis Mayta <luis@hadenlabs.com>
2021-04-10 14:32:08 -0500

    test: implement generate tags (#5)

---
+++ test/tags_basic_test.go
@@ -0,0 +1,40 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTagsBasicSuccess(t *testing.T) {
+	t.Parallel()
+
+	const NAMESPACE string = "hadenlabs"
+  const ENVIRONMENT string = "us-west"
+	const STAGE string = "development"
+	const NAME string = "jenkins"
+
+	terraformOptions := &terraform.Options{
+		// The path to where your Terraform code is located
+		TerraformDir: "tags-basic",
+		Upgrade:      true,
+		Vars: map[string]interface{}{
+			"namespace":  NAMESPACE,
+			"environment": ENVIRONMENT,
+			"stage": STAGE,
+      "name": NAME,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+	outputTags := terraform.OutputMap(t, terraformOptions, "tags")
+	outputDelimiter := terraform.Output(t, terraformOptions, "delimiter")
+	assert.NotEmpty(t, outputTags, outputTags)
+	assert.NotEmpty(t, outputDelimiter, outputDelimiter)
+}
